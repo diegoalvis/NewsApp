512d5dec039a473ce2ae8ff7608aad0e1b0d12b2
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-09-11 19:08:53 -0500

    Clean project

--- app/src/main/java/com/diegoalvis/android/newsapp/models/Article.kt
+++
@@ -1,8 +0,0 @@
-package com.diegoalvis.android.newsapp.models
-
-
-class Article(val section: String, val title: String, val url: String, val multimedia: List<Multimedia>) {
-    class Multimedia(val url: String)
-}
-
-
